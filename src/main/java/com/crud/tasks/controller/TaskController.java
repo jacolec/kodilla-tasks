112abc188724f9f96180fdc477e000f0a5826f4e
jacolec <jacwydro@gmail.com>
2022-09-11 14:02:55 +0200

    19.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,24 +1,41 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
     @GetMapping(value = "{taskId}")
     public TaskDto getTask(@PathVariable Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
+
+    @GetMapping(value = "{taskId}")
+    public Optional<Task> getTaskById(@PathVariable Long taskId) {
+        return service.getTaskById(taskId);
+    }
+
 
     @DeleteMapping
     public void deleteTask(Long taskId) {
--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -3,12 +3,24 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
 public class Task {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
