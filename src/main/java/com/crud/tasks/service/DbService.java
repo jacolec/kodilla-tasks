5f8f216243b23af01ccb936988c802a29159b399
jacolec <jacwydro@gmail.com>
2022-09-12 22:20:25 +0200

    19.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,8 +1,10 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,14 +15,24 @@ import java.util.Optional;
 public class DbService {
 
 
+    @Autowired
     private final TaskRepository repository;
 
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
 
-   public Optional<Task> getTaskById(final Long taskId) {
-        return repository.findById(taskId);
+    public Task getTask(final Long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
     }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+    public void deleteTask(final Task task) {
+        repository.delete(task);
+    }
+
+
 
 }
