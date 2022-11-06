c3ce471876e6810db7b6b1217627cf1eb120ab6a
jacolec <jacwydro@gmail.com>
2022-11-06 18:14:55 +0100

    23.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,24 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+//@EnableWebMvc
 public class TasksApplication extends SpringBootServletInitializer {
 /*
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
 	}
-
  */
-
-
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(TasksApplication.class, args);
 
-
 	}
-
 }
