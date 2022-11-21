8c7eb4306953e515c3479217399c5e6e62ad9bae
jacolec <jacwydro@gmail.com>
2022-11-21 21:05:25 +0100

    Actuator

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -4,14 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import springfox.documentation.builders.PathSelectors;
+/*import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+ */
+
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
 @EnableScheduling
 public class CoreConfiguration {
 
@@ -20,7 +22,7 @@ public class CoreConfiguration {
         return new RestTemplate();
     }
 
-    @Bean
+    /*@Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
@@ -28,4 +30,6 @@ public class CoreConfiguration {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+     */
 }
