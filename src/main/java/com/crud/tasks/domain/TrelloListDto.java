ba71a8e068c95d9e0e297045ce9d31c0264f5806
jacolec <jacwydro@gmail.com>
2022-12-10 08:50:50 +0100

    transfer to Mogenius

--- src/main/java/com/crud/tasks/domain/TrelloListDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloListDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+
 @Data
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
