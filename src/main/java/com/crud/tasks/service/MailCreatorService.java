7be3d6cf5c870f17a0aea123eef6b9fccde1a5a0
jacolec <jacwydro@gmail.com>
2022-12-11 17:13:42 +0100

    32.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class MailCreatorService {
@@ -23,6 +26,12 @@ public class MailCreatorService {
     private CompanyConfig companyConfig;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://jacolec.github.io");
@@ -32,6 +41,28 @@ public class MailCreatorService {
         context.setVariable("PREVIEW_MESSAGE", "Test preview");
         context.setVariable("BUTTON_NAME", "Go!");
         context.setVariable("GOODBYE_MESSAGE", "See you soon!");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
         return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+
+    public String sendDailyMail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://jacolec.github.io");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", companyConfig.getCompanyName());
+        context.setVariable("PREVIEW_MESSAGE", "This is your daily task preview");
+        context.setVariable("BUTTON_NAME", "Go!");
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", false);
+        return templateEngine.process("mail/daily-mail-info", context);
     }
 }
