7c6ddfbcad584b4bf56e4710be2a0fbb8a38c7d9
jacolec <jacwydro@gmail.com>
2022-12-11 16:34:15 +0100

    32.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.trello.config.AdminConfig;
+import com.crud.tasks.trello.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -18,12 +19,19 @@ public class MailCreatorService {
     @Autowired
     private AdminConfig adminConfig;
 
+    @Autowired
+    private CompanyConfig companyConfig;
+
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://jacolec.github.io");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", companyConfig.getCompanyName());
+        context.setVariable("PREVIEW_MESSAGE", "Test preview");
+        context.setVariable("BUTTON_NAME", "Go!");
+        context.setVariable("GOODBYE_MESSAGE", "See you soon!");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
